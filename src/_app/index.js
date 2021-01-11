32bd5e0d2b42879b214473836a1355743be196f5
ank3r <mwangishadrack5@gmail.com>
2021-01-11 14:03:43 +0300

    add logic for a new thread

--- src/_app/index.js
+++ src/_app/index.js
@@ -1,31 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'regenerator-runtime/runtime'
-import {login, logout} from '../utils'
-import getConfig from '../config'
-import {Button, Typography} from 'antd'
-import {LoginOutlined} from '@ant-design/icons'
-import {Layout, Menu, Row, Col, Divider, Input, Space} from 'antd'
-import {
-    SearchOutlined,
-    CommentOutlined,
-    PlusCircleFilled,
-    PlusOutlined,
-    LogoutOutlined,
-    SendOutlined
-} from '@ant-design/icons'
-import {List, ListItem, ListItemText, ListSubheader, ListItemIcon} from '@material-ui/core'
-
-const {Title, Text} = Typography
-const {Sider, Content, Header} = Layout
-const {TextArea} = Input 
-
-function ListItemLink(props){
-    return <ListItem button component="a" {...props} />
-}
+import {login} from '../utils'
+import Layout from './Layout'
+import {Row, Col, Divider} from 'antd'
+import ThreadListing from './thread/ThreadListing'
+import ThreadDetails from './thread/ThreadDetails';
 
 export default () => {
-    const [showGroupInput, setShowGroupInput] = useState(false);
-
     if(!window.walletConnection.isSignedIn()){
         // return the signin page 
         return (
@@ -44,129 +25,53 @@ export default () => {
         )
     }
 
-    return (
-        <Layout style={{minHeight: '100vh'}}>
-            <Sider collapsed={true}>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} style={{height: '100%'}}>
-                    <Menu.Item key="1" icon={<CommentOutlined />} />
-                    <Menu.Item key="2" icon={<SearchOutlined />} />
-                    <Menu.ItemGroup style={{position: 'absolute', bottom: 10, width: '100%'}}>
-                        <Menu.Divider/>
-                        <Menu.Item key={3} icon={<LogoutOutlined />} onClick={logout} />
-                    </Menu.ItemGroup>
-                    
-                </Menu>
-            </Sider>
-            <Content >
-                <Row style={{height: '100%'}}>
-                    <Col span="4">
-                        <div style={{paddingTop: '1rem'}}>
-                            <Input.Search 
-                                placeholder="Search (non-functional)"
-                            />
-                            <div style={{paddingTop:'2rem'}}>
-                                <List component="nav"
-                                    aria-labelledby="groups-listing"
-                                    subheader={
-                                        <ListSubheader component="div">
-                                            <div style={{display:'flex', justifyContent: 'space-between'}}>
-                                                <Title level={4}>Threads</Title>
-                                                <Button
-                                                    type="text"
-                                                    icon={<PlusOutlined />}
-                                                    onClick={() => setShowGroupInput(!showGroupInput)}
-                                                />
-                                            </div>
-                                        </ListSubheader>
-                                    }
-                                >
-                                    <ListItem button>
-                                        <ListItemText primary="# Welcome" />
-                                    </ListItem>
-                                    {
-                                        showGroupInput ? (
-                                            <ListItem key="110111">
-                                                <Space>
-                                                    <Input
-                                                        placeholder="Thread Name"
-                                                    />
-                                                    <Button type="primary"> Add</Button>
-                                                </Space>
-                                            </ListItem>
-                                        ): null
-                                    }
-                                </List>
-                            </div>
-                        </div>
-                    </Col>
-                    <Divider type="vertical" style={{ height: '100%', margin: 0}} />
-                    <Col span="13">
-                        <Header className="header" style={{backgroundColor: '#f0f2f5', paddingTop: '1rem'}}>
-                            <Title level={5}># Welcome</Title>
-                            <div style={{maxWidth: '15rem', display: 'flex', justifyContent: 'space-between'}}>
-                                <Text type="secondary" style={{lineHeight: '2.2rem'}}>1 Member(s)</Text>
-                                <Button type="text" icon={<PlusOutlined />}>Add member</Button>
-                            </div>
-                        </Header>
-                        <Divider />
-                            <div className="chat-area">
-                                <div className="intro-text has-text-centered" style={{
-                                    paddingTop: '2rem', 
-                                    paddingBottom: "1rem",
-                                    position: "absolute",
-                                    top: "50%",
-                                    left: "50%",
-                                    transform: "translateX(-50%) translateY(-50%)"
-                                }}>
-                                    <Title level={4}>Welcome to NEAR chat app</Title>
-                                    <Text> 
-                                        This is the welcome thread. It serves as an introduction to this awesome app. 
-                                        You can create new threads and invite friends to chat.
+    /** Fetch threads from the contract */
+    const [threads, setThreads] = useState([])
+    const [selectedThread, setSelectedThread] = useState(null)
 
-                                        To get started. Click on the '+' button next to <code>Threads</code>    
-                                    </Text>
-                                </div>
-                            </div>
-                        <div className="msg-input" style={{position: 'absolute', bottom: '1rem', width: '100%', padding: '1rem'}}>
-                            
-                            <TextArea 
-                                placeholder="Type a message"
-                                autoSize={{minRows: 3, maxRows: 4}}
-                                style={{borderRadius: 10}}
-                                disabled
-                                showCount
-                            />
-                            <Button type="primary" shape="circle" icon={<SendOutlined />} style={{position: 'absolute', bottom: 50, right: 30}}/>
-                        </div>
-                    </Col>
-                    <Divider type="vertical" style={{ height: '100%', margin: 0}}/>
-                    <Col span="6">
-                        <Header className="header" style={{backgroundColor: '#f0f2f5', paddingTop: '1rem'}}>
-                            <Title level={5}>Thread Info</Title>
-                        </Header>
-                        <Divider />
-                        <div style={{padding: '1rem'}}>
-                            <List 
-                                component="nav"
-                                aria-labelledby="member-list"
-                                subheader={
-                                    <ListSubheader>
-                                        <Title type="secondary" level={5}>MEMBERS</Title>
-                                    </ListSubheader>
-                                }
-                            >   
-                                <ListItem key="1">
-                                    <ListItemIcon>
-                                        <PlusCircleFilled />
-                                    </ListItemIcon>
-                                    <ListItemText primary="dev-1610214151818-9431498" />
-                                </ListItem>
-                            </List>
-                        </div>
-                    </Col>
-                </Row>
-            </Content>
+    useEffect(() => {
+        if(window.walletConnection.isSignedIn()){
+            // fetch the threads available for this client 
+            window.contract.get_threads({member: window.accountId})
+                .then(threads => {
+                    setThreads(threads);
+                })
+        }
+    }, [])
+
+    const selectThread = thread => setSelectedThread(thread)
+
+    // create a new thread 
+    const createThread = threadName => {
+        if(window.walletConnection.isSignedIn()){
+            // create a new thread 
+            window.contract.new_thread({topic: threadName})
+                .then(() => {
+                    // add it to threads and select it 
+                    setThreads([...threads, threadName]);
+                    setSelectedThread(threadName);
+                })
+                .catch(err => console.error(err))
+        }
+    }
+
+    return (
+        <Layout>
+            <Row style={{height: '100%'}}>
+                <Col xs={{span: 24}} lg={{span: 4}}>
+                    <ThreadListing 
+                        threads={threads}
+                        onSelect={selectThread}
+                        createNewThread={createThread}
+                    />
+                </Col>
+                <Divider type="vertical" style={{ height: '100%', margin: 0}} />
+                <Col xs={{span: 24}} lg={{span: 17}}>
+                    <ThreadDetails
+                        threadName={selectedThread}
+                    />
+                </Col>
+            </Row>
         </Layout>
     )
 }
